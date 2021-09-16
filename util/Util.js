28684257e571d3bfa1f87f34b1bf24bfe4a3ac53
WhoIAm451 <74014201+WhoIAm451@users.noreply.github.com>
2021-09-16 21:55:40 +0200

    Add files via upload

---
+++ util/Util.js
@@ -0,0 +1,30 @@
+exports.canModifyQueue = (member) => {
+  const { channelID } = member.voice;
+  const botChannel = member.guild.voice.channelID;
+
+  if (channelID !== botChannel) {
+    return;
+  }
+
+  return true;
+};
+
+let config;
+
+try {
+  config = require("../config.json");
+} catch (error) {
+  config = null;
+}
+
+exports.TOKEN = config ? config.TOKEN : process.env.TOKEN;
+exports.YOUTUBE_API_KEY = config ? config.YOUTUBE_API_KEY : process.env.YOUTUBE_API_KEY;
+exports.SOUNDCLOUD_CLIENT_ID = config ? config.SOUNDCLOUD_CLIENT_ID : process.env.SOUNDCLOUD_CLIENT_ID;
+exports.PREFIX = (config ? config.PREFIX : process.env.PREFIX) || "/";
+exports.MAX_PLAYLIST_SIZE = (config ? config.MAX_PLAYLIST_SIZE : process.env.MAX_PLAYLIST_SIZE) || 10;
+exports.PRUNING = (config ? config.PRUNING : process.env.PRUNING) || false;
+exports.STAY_TIME = (config ? config.STAY_TIME : process.env.STAY_TIME) || 30;
+exports.SPOTIFY_CLIENT_ID = config ? config.SPOTIFY_CLIENT_ID : process.env.SPOTIFY_CLIENT_ID;
+exports.SPOTIFY_SECRET_ID = config ? config.SPOTIFY_SECRET_ID : process.env.SPOTIFY_SECRET_ID;
+exports.DEFAULT_VOLUME = (config ? config.DEFAULT_VOLUME : process.env.DEFAULT_VOLUME) || 100;
+exports.LOCALE = (config ? config.LOCALE : process.env.LOCALE) || "en";
